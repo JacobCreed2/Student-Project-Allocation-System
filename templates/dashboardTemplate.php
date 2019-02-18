45beb8a4ffb798ac091453f31f135b69a8b4b831
JacobCreed2 <43750488+JacobCreed2@users.noreply.github.com>
2019-02-18 15:58:27 +0000

    Created a template for the dashboards

---
+++ templates/dashboardTemplate.php
@@ -0,0 +1,13 @@
+<?php
+   include('../resources/session.php');
+   include('../resources/styling.html');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Student Project Allocation System</title>
+</head>
+<body>
+
+</body>
+</html>
