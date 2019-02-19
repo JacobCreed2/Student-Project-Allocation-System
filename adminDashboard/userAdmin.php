653e74a9e24f656b3cb09583e5c6575bbdbf7b97
JacobCreed2 <43750488+JacobCreed2@users.noreply.github.com>
2019-02-19 18:05:10 +0000

    Creating an admin user page

---
+++ adminDashboard/userAdmin.php
@@ -0,0 +1,15 @@
+<?php
+   include('../resources/session.php');
+   include('../resources/styling.html');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Student Project Allocation System</title>
+</head>
+<?php include ('adminNavbar.php'); ?>
+
+   <body>
+      <h2><a href = "../logout.php">Sign Out</a></h2>
+   </body>
+</html>
