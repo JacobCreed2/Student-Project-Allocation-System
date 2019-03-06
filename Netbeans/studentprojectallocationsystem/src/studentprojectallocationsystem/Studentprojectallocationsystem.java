03d61e60e67b9ac69aa3fa28b3e26b6b128c4136
JacobCreed2 <43750488+JacobCreed2@users.noreply.github.com>
2019-03-06 12:43:46 +0000

    creating main class and adding methods

--- Netbeans/studentprojectallocationsystem/src/studentprojectallocationsystem/Studentprojectallocationsystem.java
+++ Netbeans/studentprojectallocationsystem/src/studentprojectallocationsystem/Studentprojectallocationsystem.java
@@ -5,6 +5,16 @@
  */
 package studentprojectallocationsystem;
 
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Scanner;
+import com.paralleldots.paralleldots.App;
+import java.util.Arrays;
+
 /**
  *
  * @author jakec
@@ -14,8 +24,14 @@ public class Studentprojectallocationsystem {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws Exception {
+    
+    Sql.runSelect();
+    
+    //Studentkeywords.runNLP();
+    
+    Studentkeywords.cleanString();
+       
     }
     
 }
