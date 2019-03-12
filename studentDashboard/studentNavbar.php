24dd3151ed40aa3bde4976a5c58998a3f6a5f948
JacobCreed2 <43750488+JacobCreed2@users.noreply.github.com>
2019-03-12 12:24:21 +0000

    Creating student dashboard

---
+++ studentDashboard/studentNavbar.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand text-white" >Student Dashboard</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav mr-auto">
+      <a class="nav-item nav-link" id="Projects" onClick="location.href='projects.php'">Projects</a>
+      <a class="nav-item nav-link" id="Supervisors" href="supervisors.php">Supervisors</a>
+    </div>
+        <div class="navbar-nav ml-auto">
+      <a class="nav-item nav-link" id="Username" onClick="location.href='userStudent.php'"><?php echo $login_session; ?></a>
+    </div>
+  </div>
+</nav>
--- studentDashboard/userStudent.php
+++ studentDashboard/userStudent.php
@@ -2,17 +2,14 @@
    include('../resources/session.php');
    include('../resources/styling.html');
 ?>
+<!DOCTYPE html>
 <html>
-   
-   <head>
-      <title>Welcome Student</title>
-   </head>
-   
+<head>
+	<title>Student Project Allocation System</title>
+</head>
 <?php include ('studentNavbar.php'); ?>
 
    <body>
-      <h1>Welcome <?php echo $login_session; ?> Student</h1>
       <h2><a href = "../logout.php">Sign Out</a></h2>
    </body>
-   
 </html>
