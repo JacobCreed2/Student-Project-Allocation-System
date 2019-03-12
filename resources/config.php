050ed2607ac5f79205fef56e0ed964cc492491dc
JacobCreed2 <43750488+JacobCreed2@users.noreply.github.com>
2019-03-12 12:35:06 +0000

    Removed remove notices function

--- resources/config.php
+++ resources/config.php
@@ -1,5 +1,5 @@
 <?php 
-	ini_set('error_reporting', E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);
+
 	define('DB_SERVER', 'localhost');
 	define('DB_USERNAME', 'root');
 	define('DB_PASSWORD', '');
