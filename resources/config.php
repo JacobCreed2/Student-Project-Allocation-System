d778f66e80983aa4c0f542e463d1ad6782a1af7e
JacobCreed2 <43750488+JacobCreed2@users.noreply.github.com>
2019-03-12 12:21:37 +0000

    Stopped notices displaying

--- resources/config.php
+++ resources/config.php
@@ -1,8 +1,9 @@
 <?php 
-   define('DB_SERVER', 'localhost');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', '');
-   define('DB_DATABASE', 'spa-db');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE)
+	ini_set('error_reporting', E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);
+	define('DB_SERVER', 'localhost');
+	define('DB_USERNAME', 'root');
+	define('DB_PASSWORD', '');
+	define('DB_DATABASE', 'spa-db');
+	$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE)
 ?>
 
