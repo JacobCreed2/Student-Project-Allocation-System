d8794536e9b05388cd4789251f20bca758ce71a9
JacobCreed2 <43750488+JacobCreed2@users.noreply.github.com>
2019-03-13 16:48:10 +0000

    Id updates to session

--- resources/session.php
+++ resources/session.php
@@ -4,11 +4,15 @@
    
    $user_check = $_SESSION['login_user'];
    
-   $ses_sql = mysqli_query($db,"select * from users where UserName = '$user_check' ");
+   $ses_sql = mysqli_query($db,"select * from users where UserName = '$user_check'");
    
    $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
    
-   $id = $row['StudentId'];
+   if (empty($row['StudentId'])) {
+      $id = $row['SupervisorId'];
+   }else{
+      $id = $row['StudentId'];
+   }
    $userName = $row['Username'];
    
    if(!isset($_SESSION['login_user'])){
