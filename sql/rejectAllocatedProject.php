bc8edeebcec2bd7b7f5b136f39f3db3409b1392c
JacobCreed2 <43750488+JacobCreed2@users.noreply.github.com>
2019-03-28 14:44:19 +0000

    Added reject project

---
+++ sql/rejectAllocatedProject.php
@@ -0,0 +1,35 @@
+<?php 
+include ('../resources/session.php');
+//if($_SERVER["REQUEST_METHOD"] == "POST") {
+  $rowId = mysqli_real_escape_string($db,$_SESSION['rowId']);//rowId is stored in the session. It's just the student ID. 
+  $supervisorId = mysqli_real_escape_string($db,$id);// supevisor Id from the session.
+  $projectId = mysqli_real_escape_string($db,$_SESSION['projectId']);// project Id stored in the session 
+  $reason = mysqli_real_escape_string($db,$_POST['reason']);// reason for rejecting the project. Posted from the form.
+  //echo $_SESSION['rowId'];
+  echo $_SESSION['login_user'];
+  echo $id;
+  $sql = "INSERT INTO rejectedprojects (StudentId, SupervisorId, ProjectId, Reason, Resolved) VALUES ('$rowId', '$supervisorId', '$projectId', '$reason', '0')";// inserts reason into rejected projects
+if ($db->query($sql) === TRUE) {
+    echo "Project updated successfully\n";
+
+} else {
+    echo "Error: " . $sql . "<br>" . $db->error;
+}
+  $sql1 = "UPDATE projects SET SupervisorId = null, Allocated = '0' WHERE StudentId = '$rowId'";
+if ($db->query($sql1) === TRUE) {
+    echo "Project updated successfully\n";
+
+} else {
+    echo "Error: " . $sql1 . "<br>" . $db->error;
+}
+  $sql2 = "UPDATE supervisors SET CurrentAllocation = CurrentAllocation - 1 WHERE Id = '$supervisorId'";
+if ($db->query($sql2) === TRUE) {
+    echo "Supervisors updated\n";
+    header('Location: http://localhost/student-project-allocation-system/adminDashboard/currentProjects.php');
+
+} else {
+    echo "Error: " . $sql2 . "<br>" . $db->error;
+}
+$db->close();
+//echo "Hello";
+?>
---
+++ sql/rejectProject.php
@@ -0,0 +1,27 @@
+<?php 
+include ('../resources/session.php');
+//if($_SERVER["REQUEST_METHOD"] == "POST") {
+  $rowId = mysqli_real_escape_string($db,$_SESSION['rowId']);//rowId is stored in the session. It's just the student ID. 
+  $supervisorId = mysqli_real_escape_string($db,$id);// supevisor Id from the session.
+  $projectId = mysqli_real_escape_string($db,$_SESSION['projectId']);// project Id stored in the session 
+  $reason = mysqli_real_escape_string($db,$_POST['reason']);// reason for rejecting the project. Posted from the form.
+  //echo $_SESSION['rowId'];
+  echo $_SESSION['login_user'];
+  echo $id;
+  $sql = "INSERT INTO rejectedprojects (StudentId, SupervisorId, ProjectId, Reason, Resolved) VALUES ('$rowId', '$supervisorId', '$projectId', '$reason', '0')";// inserts reason into rejected projects
+if ($db->query($sql) === TRUE) {
+    echo "Project updated successfully\n";
+
+} else {
+    echo "Error: " . $sql . "<br>" . $db->error;
+}
+  $sql1 = "UPDATE projects SET SupervisorId = null, Allocated = '0' WHERE StudentId = '$rowId'";
+if ($db->query($sql1) === TRUE) {
+    echo "Project updated successfully\n";
+
+} else {
+    echo "Error: " . $sql1 . "<br>" . $db->error;
+}
+$db->close();
+//echo "Hello";
+?>
