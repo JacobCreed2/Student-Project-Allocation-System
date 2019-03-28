1df8f47609c65804a335967de2e93403cfe4cbb2
JacobCreed2 <43750488+JacobCreed2@users.noreply.github.com>
2019-03-28 14:42:14 +0000

    Added button to run Algortihm

---
+++ adminDashboard/adminSettings.php
@@ -0,0 +1,16 @@
+<?php
+   include('../resources/session.php');
+   include('../resources/styling.html');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Student Project Allocation System</title>
+</head>
+<body>
+<?php include ('adminNavbar.php'); ?>
+
+<button type='button' class='btn btn-primary' onClick="window.location.href='runAllocation.php'">Start Allocation</button>
+
+</body>
+</html>
---
+++ adminDashboard/runAllocation.php
@@ -0,0 +1,3 @@
+<?php 
+exec("java -jar ../Netbeans/studentprojectallocationsystem/dist/studentprojectallocationsystem.jar", $output);
+ ?>
