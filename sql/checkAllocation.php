49cc298beca17d99893e1c8a20dbf26a3d71faa7
JacobCreed2 <43750488+JacobCreed2@users.noreply.github.com>
2019-04-03 12:27:13 +0100

    Fixed error with varible naming

--- sql/checkAllocation.php
+++ sql/checkAllocation.php
@@ -1,13 +1,12 @@
-<?php 
-$supervisorId = $id; 
-$sql = "SELECT CurrentAllocation, MaxAllocation FROM supervisors WHERE supervisors.Id = '$supervisorId'";
+<?php
+$sql = "SELECT CurrentAllocation, MaxAllocation FROM supervisors WHERE Id = '$id'";
 $result = $db->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-    	$currentAllocation = $row['currentAllocation'];
-    	$maxAllocation = $row['maxAllocation'];
+    	$currentAllocation = $row['CurrentAllocation'];
+    	$maxAllocation = $row['MaxAllocation'];
     }
 } else {
     echo "0 results";
