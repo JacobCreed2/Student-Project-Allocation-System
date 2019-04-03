5c559dc7962e2087769c57129105ca4ef69ecdf4
JacobCreed2 <43750488+JacobCreed2@users.noreply.github.com>
2019-04-03 12:31:22 +0100

    Updated id

--- tables/displayRejectedProjects.php
+++ tables/displayRejectedProjects.php
@@ -1,6 +1,5 @@
 <?php
-$supervisorId = $id; 
-$sql = "SELECT students.FirstName, students.LastName, projects.ProjectTitle, projects.ProjectDetails, Reason FROM ((rejectedprojects INNER JOIN students ON students.Id = rejectedprojects.StudentId)INNER JOIN projects ON rejectedprojects.ProjectId = projects.Id) WHERE rejectedprojects.SupervisorId = '$supervisorId'";
+$sql = "SELECT students.FirstName, students.LastName, projects.ProjectTitle, projects.ProjectDetails, Reason FROM ((rejectedprojects INNER JOIN students ON students.Id = rejectedprojects.StudentId)INNER JOIN projects ON rejectedprojects.ProjectId = projects.Id) WHERE rejectedprojects.SupervisorId = '$id'";
 $result = $db->query($sql);
 
 if ($result->num_rows > 0) {
