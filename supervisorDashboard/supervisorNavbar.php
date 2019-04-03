6e4be6361770cae3ce794bbf3b0326f754de0e82
JacobCreed2 <43750488+JacobCreed2@users.noreply.github.com>
2019-04-03 12:29:54 +0100

    Updated nav to list three tables

--- supervisorDashboard/supervisorNavbar.php
+++ supervisorDashboard/supervisorNavbar.php
@@ -1,15 +1,23 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand text-white" >Supervisor Dashboard</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+  <a class="navbar-brand text-white" >Admin Dashboard</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav mr-auto">
-      <a class="nav-item nav-link" id="Projects" onClick="location.href='projects.php'">Projects</a>
-      <a class="nav-item nav-link" id="Students" href="details.php">Add project details</a>
-    </div>
-        <div class="navbar-nav ml-auto">
-      <a class="nav-item nav-link" id="Username" onClick="location.href='userSupervisor.php'"><?php echo $userName; ?></a>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Projects
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="currentProjects.php">Projects Currently Supervising</a>
+          <a class="dropdown-item" href="pendingProjects.php">Pending Projects</a>
+          <a class="dropdown-item" href="rejectedProjects.php">Rejected Projects</a>
+        </div>
+      </li>
+    </ul>
+    <div class="navbar-nav ml-auto">
+      <a class="nav-item nav-link" id="Username" onClick="location.href='../logout.php'">Sign Out</a>
     </div>
   </div>
 </nav>
