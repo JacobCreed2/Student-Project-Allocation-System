97cdde5b8214d58d7d7dd3219d530a527dce50f3
JacobCreed2 <43750488+JacobCreed2@users.noreply.github.com>
2019-04-03 12:30:31 +0100

    Removed reject button

--- tables/displayCurrentProjects.php
+++ tables/displayCurrentProjects.php
@@ -7,16 +7,14 @@ if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
         $_SESSION['rowId'] = $row["StudentId"];
-        $_SESSION['projectId'] = $row["Id"];
         echo '<tr>';
         echo "<td>". $row["FirstName"] . " " . $row["LastName"] . "</td>";
         echo "<td>". $row["ProjectTitle"] . "</td>";
         echo "<td>". nl2br($row["ProjectDetails"]) . "</td>";
-        echo "<td><button type='button' class='btn btn-primary' data-toggle='modal' data-target='#Reject'>Reject</button></td>";
         echo "</tr>";
     }
 } else {
-    echo "0 results";
+    
 }
 $db->close();
 ?>
