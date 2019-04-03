6f0d1d8aeebcf5c28614d0d1ffe28d9c49f6ba18
JacobCreed2 <43750488+JacobCreed2@users.noreply.github.com>
2019-04-03 12:28:44 +0100

    Added if to distinguish between students who have and dont have a project

--- studentDashboard/projects.php
+++ studentDashboard/projects.php
@@ -13,7 +13,23 @@
 document.getElementById("Projects").style.color = "white";
 </script>
 <?php
+$studentId = $id;
+$sql = "SELECT ProjectTitle, ProjectDetails FROM projects WHERE StudentId = '$studentId'";
+$result = $db->query($sql);
+
+if ($result->num_rows > 0) {
+	while($row = $result->fetch_assoc()) {
+		$projectTitle = $row["ProjectTitle"];
+		$projectDetails = $row["ProjectDetails"];
+	}
+}
+if ($projectTitle == null) {
+	//echo "Show add project";
+	include ('newProject.php');
+}else{
+	//echo "Show project";
 	include ('../tables/displayStudentProjects.php');
+}
 ?>
 
 </body>
