ca4b74040835c9e8efc07bb11c4be94f60300215
JacobCreed2 <43750488+JacobCreed2@users.noreply.github.com>
2019-04-03 12:30:44 +0100

    Fixed pending projects

--- tables/displayPendingProjects.php
+++ tables/displayPendingProjects.php
@@ -1,12 +1,13 @@
 <?php
 $supervisorId = $id; 
-$sql = "SELECT students.Id, students.FirstName, students.LastName, ProjectTitle, ProjectDetails FROM projects INNER JOIN students ON projects.StudentId = students.Id WHERE projects.SupervisorId = '$supervisorId' && projects.Allocated = '0'";
+$sql = "SELECT projects.Id, students.FirstName, students.LastName, projects.StudentId, ProjectTitle, ProjectDetails FROM projects INNER JOIN students ON projects.StudentId = students.Id WHERE projects.SupervisorId = '$supervisorId' && projects.Allocated = '0'";
 $result = $db->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        $_SESSION['rowId'] = $row["Id"];
+        $_SESSION['rowId'] = $row["StudentId"];
+        $_SESSION['projectId'] = $row["Id"];
         echo '<tr>';
         echo "<td>". $row["FirstName"] . " " . $row["LastName"] . "</td>";
         echo "<td>". $row["ProjectTitle"] . "</td>";
