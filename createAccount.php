93e83240dc8f1b3e5ace4ea6d86d02d8487705cb
JacobCreed2 <43750488+JacobCreed2@users.noreply.github.com>
2019-04-03 12:24:03 +0100

    Added button to login page to create student account

---
+++ createAccount.php
@@ -0,0 +1,74 @@
+<?php
+include('./resources/styling.html');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Student Project Allocation System</title>
+</head>
+<body>
+  <form class="needs-validation" action="./sql/createStudent.php" method="post" novalidate>
+    <h2 class="text-info">Supervisors please contact the admin to get access to the system.</h2>
+    <h1>Create New Student</h1>
+    <div class="form-row">
+      <div class="col-md-4 mb-3">
+        <label for="firstName">First name</label>
+        <input type="text" class="form-control" id="firstName" name="firstname" placeholder="First name" required>
+        <div class="valid-feedback">
+          Looks good!
+        </div>
+      </div>
+    </div>
+    <div class="form-row">
+      <div class="col-md-4 mb-3">
+        <label for="lastName">Last name</label>
+        <input type="text" class="form-control" id="lastName" name="lastname" placeholder="Last name" required>
+        <div class="valid-feedback">
+          Looks good!
+        </div>
+      </div>
+    </div>
+    <div class="form-row">
+      <div class="col-md-4 mb-3">
+        <label for="lastName">Password</label>
+        <input type="password" class="form-control" id="password" name="password" placeholder="Password" required>
+        <div class="valid-feedback">
+          Looks good!
+        </div>
+      </div>
+    </div>
+    <div class="form-row">
+      <div class="col-md-4 mb-3">
+        <label for="studentId">Student Id</label>
+        <input type="text" class="form-control" id="studentId" name="studentid" placeholder="Student Id" required>
+        <label>Please note that your studentId is your username.</label>
+        <div class="valid-feedback">
+          Looks good!
+        </div>
+      </div>
+    </div>
+    <button class="btn btn-primary" type="submit">Create new student</button>
+  </form>
+
+  <script>
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
+</script>
+</body>
+</html>
