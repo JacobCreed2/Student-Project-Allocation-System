e196f06e148f23a071441a09778abc5048f9d630
JacobCreed2 <43750488+JacobCreed2@users.noreply.github.com>
2019-04-04 20:45:35 +0100

    check Allocation before adding supervisor to the list

--- sql/selectSupervisors.php
+++ sql/selectSupervisors.php
@@ -1,11 +1,13 @@
 <?php
 $getsup = "SELECT * FROM supervisors";
 $select = $db->query($getsup);
-echo '<select class="form-control" id="selectsupervisor" name="selectsupervisor" placeholder="Student Id" required>';
+echo '<select class="form-control" id="selectsupervisor" name="selectsupervisor" required>';
 echo'<option value="noselection">Please select a supervisor</option>';
 if ($select->num_rows > 0) {
   while ($row=$select->fetch_assoc()) {
-    echo'<option value="'.$row['Id'].'">'.$row['FirstName']." ".$row['LastName']."  ".$row['CurrentAllocation']."/".$row['MaxAllocation'].'</option>"';
+  	if ($row['CurrentAllocation'] < $row['MaxAllocation']) {
+  	    echo'<option value="'.$row['Id'].'">'.$row['FirstName']." ".$row['LastName']."  ".$row['CurrentAllocation']."/".$row['MaxAllocation'].'</option>"';	
+  	}
   }
   echo '</select>';
 }else{
